4e194d5e5cc7032526e9802a20a996e3172d7d72
SunlightCourtyard <kashunaiotoshi@gmail.com>
2025-03-24 16:49:12 +1100

    fix typo in querySelector, added transition time for active

--- resources/JavaScript/script.js
+++ resources/JavaScript/script.js
@@ -1,9 +1,9 @@
-const navButtons = document.querySelector('nav').querySeletorAll('a')
+const navButtons = document.querySelector('nav').querySelectorAll('a');
 
 navButtons.forEach(button => {  
-    button.addEventListener("click", function() {
+    button.addEventListener("click", function(event) {
+        event.preventDefault();
         navButtons.forEach(button => button.classList.remove("active"));
         this.classList.add("active");
-        window.alert("test");
     })
 });
