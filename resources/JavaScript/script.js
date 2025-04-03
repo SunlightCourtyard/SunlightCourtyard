0b1e00f2fefa15cc1b13b4252bf2296a953b9d5b
SunlightCourtyard <kashunaiotoshi@gmail.com>
2025-04-04 06:31:45 +1100

    add lightmode, make mode switch using button in nav, add top margin to footer as a fix of the overlap.

--- resources/JavaScript/script.js
+++ resources/JavaScript/script.js
@@ -1,4 +1,15 @@
 const navButtons = document.querySelector('nav').querySelectorAll('a');
+const lightbutton = document.querySelector('button');
+const allElements = document.querySelector('*');
+
+function lightChanger() {
+    if (document.querySelector('body').classList.contains('lightmode') == true){
+        document.querySelector('body').classList.remove('lightmode');
+    } else if (document.querySelector('body').classList.contains('lightmode') == false){
+        document.querySelector('body').classList.add('lightmode');
+    }
+}
+
 
 navButtons.forEach(button => {  
     button.addEventListener("click", function(event) {
@@ -7,3 +18,5 @@ navButtons.forEach(button => {
         this.classList.add("active");
     })
 });
+
+lightbutton.onclick = lightChanger;
